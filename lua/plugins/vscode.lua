4965259eaf74f7ee98b0c537563dfb67806182c5
Junbo Lu <johnbro@foxmail.com>
2023-09-21 17:31:39 +0800

    Add all references with <gr> in vscode

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -33,13 +33,14 @@ vim.api.nvim_create_autocmd("User", {
   pattern = "LazyVimKeymaps",
   callback = function()
     vim.opt.timeoutlen = 500 -- vscode has a slow response then nvim itself
+    vim.keymap.set("n", "H", "<cmd>call VSCodeNotify('workbench.action.previousEditor')<cr>")
+    vim.keymap.set("n", "L", "<cmd>call VSCodeNotify('workbench.action.nextEditor')<cr>")
+    vim.keymap.set("n", "gr", "<cmd>call VSCodeNotify('references-view.findReferences')")
+    vim.keymap.set("n", "<Tab>", "<cmd>call VSCodeNotify('editor.toggleFold')<cr>")
     vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
     vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
     vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
-    vim.keymap.set("n", "H", "<cmd>call VSCodeNotify('workbench.action.previousEditor')<cr>")
-    vim.keymap.set("n", "L", "<cmd>call VSCodeNotify('workbench.action.nextEditor')<cr>")
     vim.keymap.set("n", "<leader>bb", "<cmd>call VSCodeNotify('workbench.action.nextEditor')<cr>")
-    vim.keymap.set("n", "<Tab>", "<cmd>call VSCodeNotify('editor.toggleFold')<cr>")
     vim.keymap.set("n", "<localleader>ul", "<cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<cr>")
     vim.keymap.set("n", "<localleader>ur", "<cmd>call VSCodeNotify('workbench.action.toggleAuxiliaryBar')<cr>")
     vim.keymap.set("n", "<localleader>ua", "<cmd>call VSCodeNotify('workbench.action.toggleActivityBarVisibility')<cr>")
