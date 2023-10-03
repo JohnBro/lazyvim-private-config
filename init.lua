6c5dea1aaa8880f903eb3d2436500b988465c566
Johnbro <johnbro@foxmail.com>
2023-10-03 22:06:29 +0800

    Add more neovim options

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+---Join path segments that were passed as input
+---@return string
+function _G.join_paths(...)
+  local separator = package.config:sub(1, 1) -- get '/' or '\'
+
+  local args = {...}
+  local joined_path = table.concat(args, separator)
+  return joined_path
+end
+
 require("config.lazy")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,62 @@
 -- Add any additional options here
 vim.g.maplocalleader = ","
 
+vim.opt.autowriteall = true
+vim.opt.backup = true
+vim.opt.backupdir = join_paths(vim.fn.stdpath("cache"), "backup")
+vim.opt.conceallevel = 0
+vim.opt.diffopt = {
+  "internal",
+  "filler",
+  "closeoff",
+  "hiddenoff",
+  "algorithm:minimal",
+}
+vim.opt.directory = join_paths(vim.fn.stdpath("cache"), "swap")
+vim.opt.formatoptions = {
+  ["1"] = true,
+  ["2"] = true, -- Use indent from 2nd line of a paragraph
+  q = true, -- continue comments with gq"
+  c = true, -- Auto-wrap comments using textwidth
+  r = true, -- Continue comments when pressing Enter
+  n = true, -- Recognize numbered lists
+  t = false, -- Auto-wrap text using 'textwidth'
+  j = true, -- remove a comment leader when joining lines.
+  -- Only break if the line was not longer than 'textwidth' when the insert
+  -- started and only at a white character that has been entered during the
+  -- current insert command.
+  l = true,
+  v = true,
+  o = true,
+}
 vim.opt.guifont = "FiraCode Nerd Font Mono:h12" -- the font used in graphical neovim applications
+vim.opt.list = true
+vim.opt.listchars = {
+  eol = nil,
+  tab = "│ ",
+  extends = "›", -- Alternatives: … »
+  precedes = "‹", -- Alternatives: … «
+  trail = "•", -- BULLET (U+2022, UTF-8: E2 80 A2)
+}
+vim.opt.pumblend = 20
+vim.opt.undodir = join_paths(vim.fn.stdpath("cache"), "undo")
+vim.opt.wildignorecase = true
+vim.opt.wildignore = {
+  "*.aux,*.out,*.toc",
+  "*.o,*.obj,*.dll,*.jar,*.pyc,__pycache__,*.rbc,*.class",
+  -- media
+  "*.ai,*.bmp,*.gif,*.ico,*.jpg,*.jpeg,*.png,*.psd,*.webp",
+  "*.avi,*.m4a,*.mp3,*.oga,*.ogg,*.wav,*.webm",
+  "*.eot,*.otf,*.ttf,*.woff",
+  "*.doc,*.pdf",
+  -- archives
+  "*.zip,*.tar.gz,*.tar.bz2,*.rar,*.tar.xz",
+  -- temp/system
+  "*.*~,*~ ",
+  "*.swp,.lock,.DS_Store,._*,tags.lock",
+  -- version control
+  ".git,.svn",
+}
 
 -- clipboard settings
 if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
