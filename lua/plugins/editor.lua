d8995a0b1498f80551faefd3ccb123c012c14b8b
Johnbro <johnbro@foxmail.com>
2023-10-03 16:48:38 +0800

    Fix for telescope display issue

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -4,7 +4,7 @@ return {
     "nvim-telescope/telescope.nvim",
     opts = {
       defaults = {
-        layout_strategy = "horizontal",
+        layout_strategy = "vertical",
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
         winblend = 20,
